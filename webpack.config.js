c1b59f80667863c9618d71ea77b6d32e8873dcfe
Kam <kameronkales@gmail.com>
2019-04-17 16:21:53 -0400

    testing netlify config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/styles.css',
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [{
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader',
+        ],
+      }),
+    }, ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+      disable: process.env.NODE_ENV === 'development',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+}
